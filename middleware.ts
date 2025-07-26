8a1af3d39e42db8f7f871c3bb68001b1d3717b36
Fenil1828 <jasanifenil18@gmail.com>
2025-07-26 21:11:44 +0530

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-// import { NextRequest, NextResponse } from "next/server";
-// import { getSessionCookie } from "better-auth/cookies";
- 
-// export async function middleware(request: NextRequest) {
-// 	const sessionCookie = getSessionCookie(request);
- 
-// 	if (!sessionCookie) {
-// 		return NextResponse.redirect(new URL("/", request.url));
-// 	}
- 
-// 	return NextResponse.next();
-// }
- 
-// export const config = {
-// 	matcher: ["/dashboard/:path*"], // Specify the routes the middleware applies to
-// };
-=======
->>>>>>> 81769022c8fcec561769025e8fef1f379d78be96
 
 import { NextRequest, NextResponse } from "next/server";
 import { getSessionCookie } from "better-auth/cookies";
@@ -53,23 +34,3 @@ export const config = {
         '/'
     ],
 };
-
-// import { auth } from "@/lib/auth";
-// import { headers } from "next/headers";
-// import { NextRequest, NextResponse } from "next/server";
-
-// export async function middleware(request: NextRequest) {
-//     const session = await auth.api.getSession({
-//         headers: await headers()
-//     })
-
-//     if (!session) {
-//         return NextResponse.redirect(new URL("/login", request.url));
-//     }
-
-//     return NextResponse.next();
-// }
-
-// export const config = {
-//     matcher: ["/dashboard/:path*"],
-// };
