b1bea4d85cfd7eab937f9f67f14b6451ebad16f6
Fenil1828 <jasanifenil18@gmail.com>
2025-07-27 09:36:14 +0530

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -130,7 +130,7 @@ export const HeroHeader = () => {
               <div className="flex w-full flex-col space-y-3 sm:flex-row sm:gap-3 sm:space-y-0 md:w-fit">
                 <div className="flex items-center gap-1">
                   <Link
-                    href="https://github.com"
+                    href="https://github.com/Fenil1828/NoteForge/tree/master"
                     target="_blank"
                     className="touch-manipulation"
                   >
